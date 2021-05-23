2d41f0cd39ce5d1afcc20e0ce0d5d8fef98ea461
jelly-b <jelly_b@sina.com>
2021-05-23 20:17:41 +0800

    Clean the codes.

--- xeps/component/stream-accept/src/main/java/com/firstlinecode/granite/xeps/component/stream/accept/ComponentConstants.java
+++
@@ -1,6 +0,0 @@
-package com.firstlinecode.granite.xeps.component.stream.accept;
-
-public final class ComponentConstants {
-	public static final String SESSION_KEY_COMPONENT_NAME = "granite.component.name";
-	public static final String SESSION_KEY_COMPONENT_STREAM_ID = "granite.component.stream.id";
-}
--- xeps/component/stream/src/main/java/com/firstlinecode/granite/xeps/component/stream/IComponentMessageAcceptor.java
+++
@@ -1,7 +0,0 @@
-package com.firstlinecode.granite.xeps.component.stream;
-
-import com.firstlinecode.granite.framework.core.integration.IMessageReceiver;
-
-public interface IComponentMessageAcceptor extends IMessageReceiver {
-	void setComponentConnectionsRegister(IComponentConnectionsRegister connectionsRegister);
-}
