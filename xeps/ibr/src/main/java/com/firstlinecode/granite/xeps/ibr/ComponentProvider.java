982cfe9a7ee32ef33201ed7b5b2035dd6ef523c6
jelly-b <jelly_b@sina.com>
2021-06-01 12:05:24 +0800

    Make IBR plugin run correctly.

--- xeps/ibr/src/main/java/com/firstlinecode/granite/xeps/ibr/ComponentProvider.java
+++ xeps/ibr/src/main/java/com/firstlinecode/granite/xeps/ibr/ComponentProvider.java
@@ -1,7 +1,10 @@
 package com.firstlinecode.granite.xeps.ibr;
 
+import org.pf4j.Extension;
+
 import com.firstlinecode.granite.framework.core.repository.IComponentProvider;
 
+@Extension
 public class ComponentProvider implements IComponentProvider {
 
 	@Override
