3268576598e73a00f42ec674562298a0d41c547b
jelly-b <jelly_b@sina.com>
2021-06-16 12:58:58 +0800

    Remove pipes directory.

--- pipes/stream/src/main/java/com/firstlinecode/granite/pipeline/stream/IStreamNegotiant.java
+++
@@ -1,9 +0,0 @@
-package com.firstlinecode.granite.pipes.stream;
-
-import com.firstlinecode.granite.framework.core.connection.IClientConnectionContext;
-import com.firstlinecode.granite.framework.core.pipes.IMessage;
-
-public interface IStreamNegotiant {
-	void setNext(IStreamNegotiant next);
-	boolean negotiate(IClientConnectionContext context, IMessage message);
-}
